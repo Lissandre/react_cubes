0e057ab14e73356533239e8af846f6b69bed0662
Lissandre <lissandre.pasdeloup@hetic.net>
2020-11-20 10:28:20 +0100

    first commit

---
+++ src/reducers/cube.js
@@ -0,0 +1,57 @@
+import { ADD_CUBE, SHUFFLE, CHANGE_ODD, STOP_ODD } from '../constants/actions';
+
+const stateInit = {
+  number: 0,
+  cubes: [],
+  easter_egg: 17,
+}
+
+const reducer = (state = stateInit, action = {}) => {
+  let cubes
+  switch(action.type){
+    case ADD_CUBE :
+      const cube = {
+        w: 100,
+        h: 100,
+        cx: 50,
+        cy: 50,
+        r: 25,
+        number: state.number,
+        stop: false,
+      }
+      return {
+        ...state,
+        cubes : state.cubes.concat(cube),
+        number: state.number + 1,
+      }
+    case SHUFFLE:
+      cubes = state.cubes.map( cube => ({ ...cube })   )
+      cubes.sort(() => Math.random() - 0.5)
+      return {
+        ...state,
+        cubes
+      }
+    case CHANGE_ODD:
+      cubes = state.cubes.map((cube) => {
+        if (cube.number % 2 === 1) cube.stop = false
+        return { ...cube }
+      })
+      return {
+        ...state,
+        cubes,
+      }
+    case STOP_ODD:
+      cubes = state.cubes.map((cube) => {
+        if (cube.number % 2 === 1) cube.stop = true
+        return { ...cube }
+      })
+      return {
+        ...state,
+        cubes,
+      }
+    default:
+      return state
+  }
+}
+
+export default reducer
